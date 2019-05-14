cd6de8df4b7a19816db0c10d31957d07c4df3c76
koroteevmv <koroteevmv@gmail.com>
2019-05-14 15:42:21 +0400

    text loader added

--- app/src/main/java/com/example/mobdev_java_11_preferences/MainActivity.java
+++ app/src/main/java/com/example/mobdev_java_11_preferences/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.mobdev_java_11_preferences;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     EditText etText;
     Button btnSave, btnLoad;
+
+    SharedPreferences sPref;
+
+    final String SAVED_TEXT = "saved_text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +44,17 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     }
 
     void saveText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putString(SAVED_TEXT, etText.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
     }
 
     void loadText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        String savedText = sPref.getString(SAVED_TEXT, "");
+        etText.setText(savedText);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
     }
 }
