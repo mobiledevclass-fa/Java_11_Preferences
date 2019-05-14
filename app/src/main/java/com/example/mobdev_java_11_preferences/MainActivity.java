860d2a5a707ee333a61b556187f2b0749aaebb3d
koroteevmv <koroteevmv@gmail.com>
2019-05-14 15:39:21 +0400

    onClck handler added

--- app/src/main/java/com/example/mobdev_java_11_preferences/MainActivity.java
+++ app/src/main/java/com/example/mobdev_java_11_preferences/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.mobdev_java_11_preferences;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    EditText etText;
+    Button btnSave, btnLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSave:
+                saveText();
+                break;
+            case R.id.btnLoad:
+                loadText();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void saveText() {
+    }
+
+    void loadText() {
     }
 }
